e4330182335118abcefe0a662e6d6e2303866595
Aallzz <a.faraday1998@gmail.com>
2018-10-31 02:13:40 +0300

    check1

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import json
 import random
+import Math
 
 try:  # For python 3
     from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -10,6 +11,42 @@ except ImportError:  # For python 2
 
 ACTIONS = ["move", "eat", "load", "unload"]
 DIRECTIONS = ["up", "down", "right", "left"]
+MOVEID = {
+    (-1, 0): 0,
+    (1, 0): 1,
+    (0, 1): 2,
+    (0, -1): 3    
+}
+
+
+def parseJSON(json):
+    return json["map"]["height"], json["map"]["width"], json["cells"], json["id"], json["ants"]
+
+
+def getDist(x, y, xx, yy):
+    return abs(x - xx) + abs(y - yy)
+ 
+def findNearestFood(mapa, x, y):
+    res = Math.inf
+    px, py = 0, 0
+    for i, line in enumerate(mapa):
+        for j, e in enumerate(line):
+            if "food" in e:
+                res = min(res, getDist(x, y, i, j))
+                px, py = i, j
+    return px, py, res
+   
+def getDirTo(x, y, xx, yy):
+    d = Math.inf
+    rx, ry = 0, 0
+    for dx in (-1, 0, 1):
+        for dy in (-1, 0, 1):
+            if dx + dy != 1:
+                continue
+            curD = getDist(x + dx, y + dy, xx, yy)
+            if (curD < d):
+                rx, ry, d = dx, dy, curD
+    return rx, ry
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -24,14 +61,22 @@ class Handler(BaseHTTPRequestHandler):
 
         # Hive object from request payload
         hive = json.loads(payload)
+        height, width, mapa, hiveId, ants = parseJSON(hive)
+        for ant in ants:
+            x, y, d = findNearestFood(mapa, ant["x"], ant["y"])
+            dx, dy = getDirTo(ant["x"], ant["y"], x, y)
+            orders[ant] = {
+                "dir": ACTIONS[MOVEID[(dx, dy)]],
+                "act": DIRECTIONS[random.randint(0, 3)]
+            }
 
         # Loop through ants and give orders
-        orders = {}
-        for ant in hive['ants']:
-            orders[ant] = {
-                "act": ACTIONS[random.randint(0, 3)],
-                "dir": DIRECTIONS[random.randint(0, 3)]
-            }
+        # orders = {}
+        # for ant in hive['ants']:
+        #     orders[ant] = {
+        #         "act": ACTIONS[random.randint(0, 3)],
+        #         "dir": DIRECTIONS[random.randint(0, 3)]
+        #     }
         response = json.dumps(orders)
         print(response)
 
